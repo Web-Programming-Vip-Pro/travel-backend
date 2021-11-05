66b522621f0be1226ee0fcc9241f4a0c067ba172
Thanh Minh <thanhgamedev@gmail.com>
2021-11-05 16:29:48 +0700

    feat(config): add jwt secret key in env file

--- config/config.php
+++ config/config.php
@@ -4,3 +4,4 @@ define("DB_HOST", $_ENV['DB_HOST']);
 define("DB_USER", $_ENV['DB_USER']);
 define("DB_PASSWORD", $_ENV['DB_PASSWORD']);
 define("DB_TABLE", $_ENV['DB_TABLE']);
+define("JWT_SECRET", $_ENV['JWT_SECRET']);
