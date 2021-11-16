86ad488147917b9410195e2dc00d11be13900807
Thanh Minh <thanhgamedev@gmail.com>
2021-11-16 20:45:21 +0700

    fix(city): fix edit & delete req

--- app/controllers/cityController.php
+++ app/controllers/cityController.php
@@ -7,32 +7,41 @@ require_once('app/services/cityService.php');
 
 use Core\Http\BaseController;
 use App\Services\CityService;
-class cityController extends BaseController{
+
+class cityController extends BaseController
+{
     private $cityService;
-    public function __construct(){
+    public function __construct()
+    {
         $this->cityService = new CityService();
     }
     public function index()
     {
         return $this->cityService->list();
     }
-    public function postAdd(){
+    public function postAdd()
+    {
         $inputJSON = file_get_contents('php://input');
-        $req= json_decode( $inputJSON,true ); 
-       return $this->cityService->add($req);
-    }  
-    public function getEdit(){
+        $req = json_decode($inputJSON, true);
+        return $this->cityService->add($req);
+    }
+    public function getEdit()
+    {
         $id = (int)$_REQUEST['id'];
         return $this->cityService->getEdit($id);
     }
-    public function postEdit(){
+    public function postEdit()
+    {
         $inputJSON = file_get_contents('php://input');
-        $req= json_decode( $inputJSON,true ); 
-        $id = (int)$_REQUEST['id'];
-       return $this->cityService->postEdit($id,$req);
+        $req = json_decode($inputJSON, true);
+        $id = (int)$req['id'];
+        return $this->cityService->postEdit($id, $req);
     }
-    public function delete(){
-        $id = (int)$_REQUEST['id'];
-       return $this->cityService->delete($id);
+    public function delete()
+    {
+        $inputJSON = file_get_contents('php://input');
+        $req = json_decode($inputJSON, true);
+        $id = (int)$req['id'];
+        return $this->cityService->delete($id);
     }
 }
