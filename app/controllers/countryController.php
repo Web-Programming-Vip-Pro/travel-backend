21549b43ff405d7fbe9fd1616be6fc1bf60993d8
Thanh Minh <thanhgamedev@gmail.com>
2021-11-16 20:34:52 +0700

    fix(country): delete controller not get data from json

--- app/controllers/countryController.php
+++ app/controllers/countryController.php
@@ -7,32 +7,42 @@ require_once('app/services/countryService.php');
 
 use Core\Http\BaseController;
 use App\Services\CountryService;
-class countryController extends BaseController{
+
+class countryController extends BaseController
+{
     private $countryService;
-    public function __construct(){
+    public function __construct()
+    {
         $this->countryService = new CountryService();
     }
     public function index()
     {
         return $this->countryService->list();
     }
-    public function postAdd(){
+    public function postAdd()
+    {
         $inputJSON = file_get_contents('php://input');
-        $req= json_decode( $inputJSON,true ); 
+        $req = json_decode($inputJSON, true);
+        var_dump($req);
         return $this->countryService->add($req);
-    }  
-    public function getEdit(){
+    }
+    public function getEdit()
+    {
         $id = (int)$_REQUEST['id'];
         return $this->countryService->getEdit($id);
     }
-    public function postEdit(){
+    public function postEdit()
+    {
         $inputJSON = file_get_contents('php://input');
-        $req= json_decode( $inputJSON,true ); 
-        $id = (int)$_REQUEST['id'];
-        return $this->countryService->postEdit($id,$req);
+        $req = json_decode($inputJSON, true);
+        $id = (int)$req['id'];
+        return $this->countryService->postEdit($id, $req);
     }
-    public function delete(){
-        $id = (int)$_REQUEST['id'];
+    public function delete()
+    {
+        $inputJSON = file_get_contents('php://input');
+        $req = json_decode($inputJSON, true);
+        $id = (int)$req['id'];
         return $this->countryService->delete($id);
     }
 }
