396d85c65f77987edee7d9613aedd19955e1b94e
Thanh Minh <thanhgamedev@gmail.com>
2021-11-18 15:10:33 +0700

    fix(middleware): use isset instead of access property

--- app/middleware/middleware.php
+++ app/middleware/middleware.php
@@ -1,62 +1,79 @@
 <?php
 
 namespace App\Middleware;
+
 require_once('app/services/authenticationService.php');
+
 use App\Services\AuthenticationService;
 
-class Middleware {
+class Middleware
+{
     private $authenticationService;
-    public function __construct(){
+    public function __construct()
+    {
         $this->authenticationService = new AuthenticationService();
     }
-    public function handle(){
-        $authHeader = apache_request_headers()['Authorization'];
-        if(!isset($authHeader)){
+    public function handle()
+    {
+        $authHeader = isset(apache_request_headers()['Authorization']) ? apache_request_headers()['Authorization'] : null;
+        if (!isset($authHeader)) {
             return false;
         }
         $arr = explode(" ", $authHeader);
         $token = $arr[1];
         $jwt = $this->authenticationService->decodeJWTToken($token);
-        if($jwt->user == null){
+        if (!isset($jwt->user)) {
             return false;
         }
         return $jwt->user;
     }
-    public function handleAdmin(){
-        $authHeader = apache_request_headers()['Authorization'];
-        if(!isset($authHeader)){
+    public function handleAdmin()
+    {
+        $authHeader = isset(apache_request_headers()['Authorization']) ? apache_request_headers()['Authorization'] : null;
+        if (!isset($authHeader)) {
             return false;
         }
         $arr = explode(" ", $authHeader);
         $token = $arr[1];
         $jwt = $this->authenticationService->decodeJWTToken($token);
-        if($jwt->user->role == 2){
+        if (!isset($jwt->user)) {
+            return false;
+        }
+        if ($jwt->user->role == 2) {
             return $jwt->user;
         }
         return false;
     }
-    public function handleAgency(){
-        $authHeader = apache_request_headers()['Authorization'];
-        if(!isset($authHeader)){
+    public function handleAgency()
+    {
+        $authHeader = isset(apache_request_headers()['Authorization']) ? apache_request_headers()['Authorization'] : null;
+        if (!isset($authHeader)) {
             return false;
         }
         $arr = explode(" ", $authHeader);
         $token = $arr[1];
         $jwt = $this->authenticationService->decodeJWTToken($token);
-        if($jwt->user->role != 0){
+        if (!isset($jwt->user)) {
+            return false;
+        }
+        if ($jwt->user->role != 0) {
             return $jwt->user;
         }
         return false;
     }
-    public function handleUser(){
+    public function handleUser()
+    {
         $authHeader = apache_request_headers()['Authorization'];
-        if(!isset($authHeader)){
+        if (!isset($authHeader)) {
             return false;
         }
         $arr = explode(" ", $authHeader);
         $token = $arr[1];
         $jwt = $this->authenticationService->decodeJWTToken($token);
-        if($jwt->user->role == 0){
+        if (!isset($jwt->user)) {
+            return false;
+        }
+        if ($jwt->user->role == 0) {
             return $jwt->user;
         }
         return false;
