f6ead287bc7ca56e8d0b391d7743b424e2375e96
Thanh Minh <thanhgamedev@gmail.com>
2021-11-19 14:57:06 +0700

    fix(country): remove echo sql service

--- app/models/countryModel.php
+++ app/models/countryModel.php
@@ -1,30 +1,38 @@
 <?php
+
 namespace App\Models;
+
 include_once('lib/database.php');
+
 use Database\DB;
 
-class CountryModel {
+class CountryModel
+{
     public $conn;
     private $table = 'tb_country';
-    public function __construct(){
+    public function __construct()
+    {
         $this->conn = new DB();
     }
-    public function get ($id = -1,$page=0,$limit=20){
-        if($id == -1){
+    public function get($id = -1, $page = 0, $limit = 20)
+    {
+        if ($id == -1) {
             $firstRow = $page * $limit;
-            $sql = 'SELECT * FROM '.$this->table.' LIMIT '.$firstRow.','.$limit;
-            echo $sql;
+            $sql = 'SELECT * FROM ' . $this->table . ' LIMIT ' . $firstRow . ',' . $limit;
             return $this->conn->query($sql);
         }
-        return $this->conn->getRowArray($this->table,$id);
+        return $this->conn->getRowArray($this->table, $id);
     }
-    public function create($data){
-       return $this->conn->insert($this->table,$data);
+    public function create($data)
+    {
+        return $this->conn->insert($this->table, $data);
     }
-    public function update($id,$data){
-        return $this->conn->update($this->table,$data,$id);
+    public function update($id, $data)
+    {
+        return $this->conn->update($this->table, $data, $id);
     }
-    public function delete($id){
-        return $this->conn->delete($this->table,$id);
+    public function delete($id)
+    {
+        return $this->conn->delete($this->table, $id);
     }
 }
