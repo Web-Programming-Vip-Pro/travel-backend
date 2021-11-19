2cf695e5c0cc6eb8e77a5e617bc48205c6ee1953
Thanh Minh <thanhgamedev@gmail.com>
2021-11-19 11:37:08 +0700

    feat(authentication): update authentication

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 require_once('app/services/authenticationService.php');
 require_once('app/services/userService.php');
+
 use Core\Http\BaseController;
 use App\Services\UserService;
 
@@ -15,27 +16,27 @@ class userController extends BaseController
         $this->userService = new UserService();
     }
     public function index()
-    {   
+    {
         /**
          * middleware user
          */
-       return $this->userService->index();
+        return $this->userService->index();
     }
     public function list()
-    {   
+    {
         // when accessed,get data users
         return $this->userService->list();
     }
     public function postAdd()
     {
         $inputJSON = file_get_contents('php://input');
-        $req= json_decode( $inputJSON,true ); 
+        $req = json_decode($inputJSON, true);
         return $this->userService->add($req);
     }
     public function getEdit()
-    {   
+    {
         $inputJSON = file_get_contents('php://input');
-        $req= json_decode( $inputJSON,true );
+        $req = json_decode($inputJSON, true);
         $id = (int)$req['id'];
         return $this->userService->getEdit($id);
     }
@@ -47,47 +48,47 @@ class userController extends BaseController
     public function postEdit()
     {
         $inputJSON = file_get_contents('php://input');
-        $req= json_decode( $inputJSON,true );
+        $req = json_decode($inputJSON, true);
         $id = (int)$req['id'];
-        return $this->userService->postEdit($id,$req);
+        return $this->userService->postEdit($id, $req);
     }
     public function updateInfo()
     {
         $inputJSON = file_get_contents('php://input');
-        $req= json_decode( $inputJSON,true );
+        $req = json_decode($inputJSON, true);
         $id = (int)$req['id'];
         var_dump($req);
-        return $this->userService->updateInfo($id,$req);
+        return $this->userService->updateInfo($id, $req);
     }
     public function delete()
     {
         $inputJSON = file_get_contents('php://input');
-        $req= json_decode( $inputJSON,true );
+        $req = json_decode($inputJSON, true);
         $id = (int)$req['id'];
         return $this->userService->delete($id);
     }
-    public function login(){
+    public function login()
+    {
         $inputJSON = file_get_contents('php://input');
-        $req= json_decode( $inputJSON,true ); 
+        $req = json_decode($inputJSON, true);
         return $this->userService->login($req);
     }
     public function register()
     {
         $inputJSON = file_get_contents('php://input');
-        $req= json_decode( $inputJSON,true ); 
+        $req = json_decode($inputJSON, true);
         return $this->userService->register($req);
     }
     public function forget()
     {
         $inputJSON = file_get_contents('php://input');
-        $req= json_decode( $inputJSON,true ); 
+        $req = json_decode($inputJSON, true);
         return $this->userService->forget($req);
     }
     public function changePassword()
     {
         $inputJSON = file_get_contents('php://input');
-        $req= json_decode( $inputJSON,true ); 
+        $req = json_decode($inputJSON, true);
         return $this->userService->changePassword($req);
     }
 }
- 
--- app/services/userService.php
+++ app/services/userService.php
@@ -2,7 +2,7 @@
 
 namespace App\Services;
 
-require_once('storage/helper.php'); 
+require_once('storage/helper.php');
 require_once('core/http/Container.php');
 require_once('app/models/userModel.php');
 require_once('app/services/authenticationService.php');
@@ -17,6 +17,7 @@ use Core\Http\BaseController;
 use App\Middleware\Middleware;
 use App\Services\AuthenticationService;
 use App\Validator\UserValidate;
+
 class UserService
 {
     private $helper;
@@ -38,41 +39,43 @@ class UserService
         $this->userMiddle         = $this->middleware->handle();
         $this->adminMiddle      = $this->middleware->handleAdmin();
     }
-    public function index(){
+    public function index()
+    {
         $authHeader = apache_request_headers()['Authorization'];
-        if(!isset($authHeader)){
+        if (!isset($authHeader)) {
             return null;
         }
         $arr = explode(" ", $authHeader);
         $token = $arr[1];
         $jwt = $this->authenticationService->decodeJWTToken($token);
-        if($jwt == null){
+        if ($jwt == null) {
             $msg =  'You are not loged in';
-            return $this->controller->status(401,$msg);
-        }  
+            return $this->controller->status(401, $msg);
+        }
         // when accessed,get data users
-        return $this->controller->status(200,$jwt);
+        return $this->controller->status(200, $jwt);
     }
-    public function list(){
+    public function list()
+    {
         $result = $this->user->get();
-        return $this->controller->status(200,$result);
+        return $this->controller->status(200, $result);
     }
     public function add($req)
     {
-        if($this->adminMiddle == false){
-            return $this->controller->status(401,"Unauthorized");
+        if ($this->adminMiddle == false) {
+            return $this->controller->status(401, "Unauthorized");
         }
-        $msg = $this->handleValidator($req,'add');
-        if($msg != false){
-            return $this->controller->status(422,$msg);
+        $msg = $this->handleValidator($req, 'add');
+        if ($msg != false) {
+            return $this->controller->status(422, $msg);
         }
         $resultByEmail = $this->user->getByEmail($req['email']);
-        if($resultByEmail != null){
-            $msg='User existed';
-            return $this->controller->status(500,$msg);
+        if ($resultByEmail != null) {
+            $msg = 'User existed';
+            return $this->controller->status(500, $msg);
         }
         $hashed_password = password_hash($req["password"], PASSWORD_DEFAULT);
-        $data =[
+        $data = [
             "name"              => $req["name"],
             "email"             => $req["email"],
             "password"          => $hashed_password,
@@ -84,189 +87,194 @@ class UserService
         $data['info'] = $this->helper->jsonEncodeInfo($req);
         $data['social'] = $this->helper->jsonEncodeSocial($req);
         $result = $this->user->create($data);
-        if($result == true){
+        if ($result == true) {
             $payload = $this->user->getByEmail($data['email']);
             $JWT = $this->authenticationService->generateJWTToken($payload);
             $msg = $JWT;
-            return $this->controller->status(201,$msg);
+            return $this->controller->status(201, $msg);
         }
         $msg = 'Add user to database fail';
-        return $this->controller->status(500,$msg);
+        return $this->controller->status(500, $msg);
     }
-    public function getEdit($id){
-        if($this->userMiddle == false){
-            return $this->controller->status(401,"Unauthorized");
+    public function getEdit($id)
+    {
+        if ($this->userMiddle == false) {
+            return $this->controller->status(401, "Unauthorized");
         }
         $msg = $this->handleId($id);
-        if($msg != false){
-            return $this->controller->status(500,$msg);
+        if ($msg != false) {
+            return $this->controller->status(500, $msg);
         }
         $result = $this->user->get($id);
-        return $this->controller->status(200,$result);
+        return $this->controller->status(200, $result);
     }
-    public function postEdit($id,$req)
+    public function postEdit($id, $req)
     {
-        if($this->userMiddle == false){
-            return $this->controller->status(401,"Unauthorized");
+        if ($this->userMiddle == false) {
+            return $this->controller->status(401, "Unauthorized");
         }
         $msg = $this->handleId($id);
-        if($msg != false){
-            return $this->controller->status(500,$msg);
+        if ($msg != false) {
+            return $this->controller->status(500, $msg);
         }
-        $msg = $this->handleValidator($req,'edit');
-        if($msg != false){
-            return $this->controller->status(422,$msg);
+        $msg = $this->handleValidator($req, 'edit');
+        if ($msg != false) {
+            return $this->controller->status(422, $msg);
         }
-        $data =[
+        $data = [
             "name"          => $req["name"],
             "bio"           => $req['bio'],
         ];
-        if(isset($req['avatar'])){
+        if (isset($req['avatar'])) {
             $data['avatar']  = $req['avatar'];
         }
-        if(isset($req['image_cover'])){
+        if (isset($req['image_cover'])) {
             $data['image_cover']  = $req['image_cover'];
         }
         $data['info'] = $this->helper->jsonEncodeInfo($req);
         $data['social'] = $this->helper->jsonEncodeSocial($req);
-        if($this->userMiddle->role == 2){
-            if(isset($req['role'])){
+        if ($this->userMiddle->role == 2) {
+            if (isset($req['role'])) {
                 $data['role']           = $req['role'];
             }
-            if(isset($req['status_agency'])){
+            if (isset($req['status_agency'])) {
                 $data['status_agency']  = $req['status_agency'];
             }
-            if(isset($req['blocked'])){
+            if (isset($req['blocked'])) {
                 $data['blocked']        = $req['blocked'];
             }
         }
         // nếu có password mới update
-        if(isset($req['password']) && $req['password'] == $req['repassword']){
+        if (isset($req['password']) && $req['password'] == $req['repassword']) {
             $hashed_password = password_hash($req["password"], PASSWORD_DEFAULT);
             $data['password'] = $hashed_password;
         }
-        $result = $this->user->update($id,$data);
-        if($result == true){
+        $result = $this->user->update($id, $data);
+        if ($result == true) {
             $msg = 'Update user success';
-            return $this->controller->status(200,$msg);
+            return $this->controller->status(200, $msg);
         }
         $msg = 'Update user error';
-        return $this->controller->status(500,$msg);
+        return $this->controller->status(500, $msg);
     }
-    public function updateInfo($id,$req){
-        if($this->userMiddle == false){
-            return $this->controller->status(401,"Unauthorized");
+    public function updateInfo($id, $req)
+    {
+        if ($this->userMiddle == false) {
+            return $this->controller->status(401, "Unauthorized");
         }
-        $msg = $this->handleValidator($req,'edit');
-        if($msg != false){
-            return $this->controller->status(422,$msg);
+        $msg = $this->handleValidator($req, 'edit');
+        if ($msg != false) {
+            return $this->controller->status(422, $msg);
         }
-        $data =[
+        $data = [
             "name"          => $req["name"],
             "bio"           => $req['bio'],
         ];
-        if(isset($req['avatar'])){
+        if (isset($req['avatar'])) {
             $data['avatar']  = $req['avatar'];
         }
-        if(isset($req['image_cover'])){
+        if (isset($req['image_cover'])) {
             $data['image_cover']  = $req['image_cover'];
         }
         $data['info'] = $this->helper->jsonEncodeInfo($req);
         $data['social'] = $this->helper->jsonEncodeSocial($req);
         $result = $this->user->get($id);
-        if(isset($req['password']) && $req['email'] != $result['email']){
-            if(!password_verify($req['password'], $result['password'])) {
+        if (isset($req['password']) && $req['email'] != $result['email']) {
+            if (!password_verify($req['password'], $result['password'])) {
                 $msg = 'Password incorrect';
-                return $this->controller->status(500,$msg);
-            } 
+                return $this->controller->status(500, $msg);
+            }
             $resultByEmail = $this->user->getByEmail($req['email']);
-            if($resultByEmail != null){
-                $msg='User existed';
-                return $this->controller->status(500,$msg);
+            if ($resultByEmail != null) {
+                $msg = 'User existed';
+                return $this->controller->status(500, $msg);
             }
             $data['email'] = $req['email'];
         }
-        $result = $this->user->update($id,$data);
-        if($result == true){
+        $result = $this->user->update($id, $data);
+        if ($result == true) {
             $msg = 'Update user success';
-            return $this->controller->status(200,$msg);
+            return $this->controller->status(200, $msg);
         }
         $msg = 'Update user error';
-        return $this->controller->status(500,$msg);
+        return $this->controller->status(500, $msg);
     }
-    public function delete ($id){
-        if($this->adminMiddle  == false){
-            return $this->controller->status(401,"Unauthorized");
+    public function delete($id)
+    {
+        if ($this->adminMiddle  == false) {
+            return $this->controller->status(401, "Unauthorized");
         }
         $msg = $this->handleId($id);
-        if($msg != false){
-            return $this->controller->status(500,$msg);
+        if ($msg != false) {
+            return $this->controller->status(500, $msg);
         }
         $this->user->delete($id);
         $msg = 'Delete user success';
-        return $this->controller->status(200,$msg);
+        return $this->controller->status(200, $msg);
     }
-    public function login($req){
-        $msg = $this->handleValidator($req,'login');
-        if($msg != false){
-            return $this->controller->status(422,$msg);
+    public function login($req)
+    {
+        $msg = $this->handleValidator($req, 'login');
+        if ($msg != false) {
+            return $this->controller->status(422, $msg);
         }
         $email = $req['email'];
         $password = $req['password'];
         $resultByEmail = $this->user->getByEmail($email);
-        if($resultByEmail == null){
+        if ($resultByEmail == null) {
             $msg =  'User not existed';
-            return $this->controller->status(500,$msg);
+            return $this->controller->status(500, $msg);
         }
         $passwordHash = $resultByEmail[0]->password;
         // $verify =password_verify($password, $passwordHash;
-        if(!password_verify($password, $passwordHash)) {
+        if (!password_verify($password, $passwordHash)) {
             $msg = 'Password incorrect';
-            return $this->controller->status(500,$msg);
+            return $this->controller->status(500, $msg);
         }
-        /// Here we will transform this array into JWT:
-        $jwt = $this->authenticationService->generateJWTToken($resultByEmail);
-        $msg = $jwt;
-        return $this->controller->status(200,$msg);
+        unset($resultByEmail[0]->password);
+        return $this->controller->status(200, $resultByEmail[0]);
     }
-    public function register($req){
-        $msg = $this->handleValidator($req,'register');
-        if($msg != false){
-            return $this->controller->status(422,$msg);
+    public function register($req)
+    {
+        $msg = $this->handleValidator($req, 'register');
+        if ($msg != false) {
+            return $this->controller->status(422, $msg);
         }
         $hashed_password = password_hash($req["password"], PASSWORD_DEFAULT);
-        $data =[
+        $data = [
             "name"              => $req["name"],
             "email"             => $req["email"],
             "password"          => $hashed_password,
         ];
         $resultByEmail = $this->user->getByEmail($data['email']);
-        if($resultByEmail != null){
-            $msg='User existed';
-            return $this->controller->status(500,$msg);
+        if ($resultByEmail != null) {
+            $msg = 'User existed';
+            return $this->controller->status(500, $msg);
         }
         $result = $this->user->create($data);
-        if($result == true){
+        if ($result == true) {
             $payload = $this->user->getByEmail($data['email']);
-            $JWT = $this->authenticationService->generateJWTToken($payload);
-            $msg = $JWT;
-            return $this->controller->status(201,$msg);
+            unset($payload[0]->password);
+            // $JWT = $this->authenticationService->generateJWTToken($payload);
+            // $msg = $JWT;
+            return $this->controller->status(201, $payload[0]);
         }
         $msg = 'Add user to database fail';
-        return $this->controller->status(500,$msg);
+        return $this->controller->status(500, $msg);
     }
-    public function forget($req){
-        $msg = $this->handleValidator($req,'forget');
-        if($msg != false){
-            return $this->controller->status(422,$msg);
+    public function forget($req)
+    {
+        $msg = $this->handleValidator($req, 'forget');
+        if ($msg != false) {
+            return $this->controller->status(422, $msg);
         }
         $result = $this->user->getByEmail($req['email']);
-        if($result == null){
-            return $this->controller->status(500,"User not exactly");
+        if ($result == null) {
+            return $this->controller->status(500, "User not exactly");
         }
         $passReset = $this->helper->generateRandomString();
         $toMail = $req['email'];
-        $subject = "Forget Password with " .$req['email'];
+        $subject = "Forget Password with " . $req['email'];
         $body = '<!DOCTYPE html>
         <html lang="en">
         <head>
@@ -277,81 +285,84 @@ class UserService
         </head>
         <body>
             <h1>Your password reset.Please login with new password and change it soon.</h1>
-            <p>New Password :' .$passReset. '</p>
+            <p>New Password :' . $passReset . '</p>
         </body>
         </html>';
-        $success = $this->sendMail->sendMail($toMail,$subject,$body);
-        if(!$success){
-            return $this->controller->status(500,"Send mail fail");
+        $success = $this->sendMail->sendMail($toMail, $subject, $body);
+        if (!$success) {
+            return $this->controller->status(500, "Send mail fail");
         }
         $data = null;
         $hashed_password = password_hash($passReset, PASSWORD_DEFAULT);
         $data['password'] = $hashed_password;
-        $result = $this->user->update((int)$result['id'],$data);
-        if($result == true){
+        $result = $this->user->update((int)$result['id'], $data);
+        if ($result == true) {
             $msg = 'Password sent to email.Please check email to using new password';
-            return $this->controller->status(200,$msg);
+            return $this->controller->status(200, $msg);
         }
         $msg = 'Reset password fail';
-        return $this->controller->status(500,$msg);
+        return $this->controller->status(500, $msg);
     }
-    public function changePassword($req){
-        if($this->userMiddle == false){
-            return $this->controller->status(401,"Unauthorized");
+    public function changePassword($req)
+    {
+        if ($this->userMiddle == false) {
+            return $this->controller->status(401, "Unauthorized");
         }
-        $msg = $this->handleValidator($req,'changePassword');
-        if($msg != false){
-            return $this->controller->status(422,$msg);
+        $msg = $this->handleValidator($req, 'changePassword');
+        if ($msg != false) {
+            return $this->controller->status(422, $msg);
         }
         $oldPassword = $req['oldPassword'];
         // $verify =password_verify($password, $passwordHash;
-        if(!password_verify($oldPassword, $this->userMiddle->password)) {
+        if (!password_verify($oldPassword, $this->userMiddle->password)) {
             $msg = 'Old password incorrect';
-            return $this->controller->status(500,$msg);
+            return $this->controller->status(500, $msg);
         }
         $data['password'] = $req['newPassword'];
-        $result = $this->user->update((int)$this->userMiddle->id,$data);
-        if($result == true){
+        $result = $this->user->update((int)$this->userMiddle->id, $data);
+        if ($result == true) {
             $msg = 'Change password success';
-            return $this->controller->status(200,$msg);
+            return $this->controller->status(200, $msg);
         }
         $msg = 'Change password fail';
-        return $this->controller->status(500,$msg);
+        return $this->controller->status(500, $msg);
     }
-    public function handleValidator($req,$action){
+    public function handleValidator($req, $action)
+    {
         $msgs = null;
-        if($action == 'add'){
-            $msgs = $this->validate->add($req); 
+        if ($action == 'add') {
+            $msgs = $this->validate->add($req);
         }
-        if($action == 'edit'){
-            $msgs = $this->validate->edit($req); 
+        if ($action == 'edit') {
+            $msgs = $this->validate->edit($req);
         }
-        if($action == 'forget'){
-            $msgs = $this->validate->forget($req); 
+        if ($action == 'forget') {
+            $msgs = $this->validate->forget($req);
         }
-        if($action == 'login'){
-            $msgs = $this->validate->login($req); 
+        if ($action == 'login') {
+            $msgs = $this->validate->login($req);
         }
-        if($action == 'changePassword'){
-            $msgs = $this->validate->changePassword($req); 
+        if ($action == 'changePassword') {
+            $msgs = $this->validate->changePassword($req);
         }
-        if($action == 'register'){
-            $msgs = $this->validate->register($req); 
+        if ($action == 'register') {
+            $msgs = $this->validate->register($req);
         }
-        if(count($msgs) > 0){
+        if (count($msgs) > 0) {
             return $msgs;
-        } 
+        }
         return false;
     }
-    public function sendMail(){
-
+    public function sendMail()
+    {
     }
-    public function handleId($id){
-        if($id == 0){
+    public function handleId($id)
+    {
+        if ($id == 0) {
             return 'Id not fill in';
         }
         $resultGetById = $this->user->get($id);
-        if($resultGetById == null){
+        if ($resultGetById == null) {
             return  'Id not exactly';
         }
         return false;
