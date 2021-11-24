078fe51000ef07b1dcfbab286cfcb59eeaa2d644
Thanh Minh <thanhgamedev@gmail.com>
2021-11-25 00:55:56 +0700

    feat(helper): add condition helper

---
+++ helpers/condition.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Helpers;
+// create class to build sql condition
+class Condition
+{
+    private $_condition = '';
+
+    // get condition
+    public function getCondition()
+    {
+        return $this->_condition;
+    }
+
+    public function addCondition($field, $value, $operator = '=', $is_or = false)
+    {
+        if ($this->_condition != '') {
+            if ($is_or) {
+                $this->_condition .= ' OR ';
+            } else {
+                $this->_condition .= ' AND ';
+            }
+        }
+        $this->_condition .= "$field $operator $value";
+    }
+
+    public function isOr($type)
+    {
+        if ($type == 'or') {
+            return true;
+        }
+        return false;
+    }
+}
